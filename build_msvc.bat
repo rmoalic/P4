a8453efb8666b6354062f13923315a58910bb8b3
Robin Moalic <robin.moalic@yahoo.fr>
2021-03-01 11:23:22 +0100

    added p4 in windows build

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set CFLAGS=/O3
-set INCLUDES=/I SDL2\include SDL2_ttf\include
+set INCLUDES=/I SDL2\include /I SDL2_ttf\include /I .
 set LIBS=SDL2\lib\x64\SDL2.lib SDL2\lib\x64\SDL2main.lib SDL2_ttf\lib\x64\SDL2_ttf.lib Shell32.lib
 
-cl.exe %CXXFLAGS% %INCLUDES% main.c /link %LIBS% -SUBSYSTEM:windows
+cl.exe %CXXFLAGS% %INCLUDES% p4.c main.c /link %LIBS% -SUBSYSTEM:windows
