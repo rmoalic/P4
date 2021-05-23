8e123eebba0038361f4c8126a3459d390977fc95
Robin Moalic <robin.moalic@yahoo.fr>
2021-05-23 20:04:38 +0200

    fix: cflags not used

--- build_msvc.bat
+++ build_msvc.bat
@@ -7,7 +7,7 @@ set LIBS=SDL2\lib\x64\SDL2.lib SDL2\lib\x64\SDL2main.lib SDL2_ttf\lib\x64\SDL2_t
 mkdir out
 
 rc.exe ressource.rc
-cl.exe %CXXFLAGS% %INCLUDES% p4.c main.c /link %LIBS% ressource.res -SUBSYSTEM:windows /out:out/main.exe
+cl.exe %CFLAGS% %INCLUDES% p4.c main.c /link %LIBS% ressource.res -SUBSYSTEM:windows /out:out/main.exe
 
 copy *.ttf out
 copy SDL2\lib\x64\*.dll out
